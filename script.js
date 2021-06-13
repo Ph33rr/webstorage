5bd0199922af3a582cb896943ae853a7f55be203
Ph33rr <77519024+Ph33rr@users.noreply.github.com>
2021-06-13 08:24:29 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const ul = document.querySelector('ul');
+const input = document.getElementById('item');
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+let Ph33r = ["test1","test2","test3","test5"]
+itemsArray.forEach(liMaker);
+if(!localStorage.getItem('items')){localStorage.setItem('items', JSON.stringify(Ph33r))};
+function liMaker(text){
+  const li = document.createElement('li');
+  li.innerHTML = text;
+  ul.appendChild(li);
+}
+//ar-SA-sticky-header-8
+function add(){
+  itemsArray.push(input.value);
+  localStorage.setItem('items', JSON.stringify(itemsArray));
+  liMaker(input.value);
+  input.value = '';
+}
+
+function del(){
+  localStorage.clear();
+  ul.innerHTML = '';
+  itemsArray = [];
+}
