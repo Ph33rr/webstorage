a679e54f4a65b518c2b3fb64625808e3e295b613
Ph33rr <77519024+Ph33rr@users.noreply.github.com>
2021-06-13 20:43:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 const ul = document.querySelector('ul');
 const input = document.getElementById('item');
 let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
-let Ph33r = ["test1","test2","test3","test5"]
 itemsArray.forEach(liMaker);
+let Ph33r = ["test1","test2","test3","test5"]
 if(!localStorage.getItem('items')){localStorage.setItem('items', JSON.stringify(Ph33r))};
+
 function liMaker(text){
   const li = document.createElement('li');
   li.innerHTML = text;
   ul.appendChild(li);
 }
-/*
+
 function add(){
   itemsArray.push(input.value);
   localStorage.setItem('items', JSON.stringify(itemsArray));
   liMaker(input.value);
   input.value = '';
 }
-*/
+
 function del(){
   localStorage.clear();
   ul.innerHTML = '';
